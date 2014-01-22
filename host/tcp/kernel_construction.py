3a2f1067025725e0e0ba4e36a1370054e65dd6b4
vishwa91 <vishwa.hyd@gmail.com>
2014-01-22 07:53:57 +0530

    Writing down linear equations is not so easy.

--- host/tcp/kernel_construction.py
+++ host/tcp/kernel_construction.py
@@ -10,6 +10,7 @@ from numpy import fft
 import Image
 
 accel_data_file = '../output/cam/saved_ac.dat'
+t = 10e-3
 
 def estimate_g(data):
     '''Estimate gravity vector using projection onto unit sphere
@@ -30,9 +31,45 @@ def _estimate_position(accel_vector, nestim=5):
     nvar = xlen/nestim
     # our objective function is min. Xt.Q.X + Ct.X where,
     # X = [a1, a2, ...., b1, b2....., c1, c2, .....]
-    temp = eye(xlen)    # Stuck here.
-    C = -2*accel_vector
+    temp = eye(nvar)     
+    Q = zeros((3*nvar, 3*nvar))
+    Q[2*nvar:, 2*nvar:] = temp
+    C = zeros((3*nvar, 1))
+    for i in range(0, xlen, nestim):
+        C[2*nvar+i/nestim, 0] = sum(accel_vector[i:i+nestim])
+    # E is our constrain matrix. It is a 2nvar x 3nvar matrix.
+    E = zeros((2*nvar, 3*nvar))
+    # RHS of the constrain.
+    d = zeros((2*nvar, 1))
     
+    # Create constrain matrix.
+    for i in range(0, nvar):
+        # ai are 0->nvar-1; bi are nvar->2nvar-1, ci are 2nvar->3nvar-1 
+
+        # First nvar equations are position continuity equations and the
+        # next nvar equations are velocity continuity equations.
+
+        # Position continuity equation:
+        # ai - ai+1 + bi(ti-nestim*t*i) - bi+1(-nestim*t*i+1) +
+        # ci(tj-nestim*t*j)^2 - ci+1(-nestim*t*i+1)^2 = 0
+        E[i, i] += 1; E[i, i+1] += -1;
+        E[i, nvar+i] += t*i - nestim*t*i; E[i, nvar+i+1] += nestim*t*(i+1);
+        E[i, 2*nvar+i] += (t*i - nestim*t*i)**2;
+        E[i, 2*nvar+i+1] -= (nestim*t*(i+1))**2
+
+        # Velocity continuity equation:
+        # bi - bi+1 + ci(ti-nestim*t*i) - ci+1(-nestim*t*i+1) = 0
+        E[i+nvar, i+nvar] += 1; E[i+nvar, i+nvar+1] -= 1;
+        E[i+nvar, i+2*nvar] += t*i - nestim*t*i;
+        E[i+nvar, i+2*nvar+1] += nestim*t*(i+1);
+    # Now that we have all our matrices, we can form the final matrix.
+    augmented_mat = zeros((5*nvar, 5*nvar))
+    augmented_mat[:3*nvar, :3*nvar] = Q
+    augmented_mat[3*nvar:, :3*nvar] = E
+    augmented_mat[:3*nvar, 3*nvar:] = E.T
+    # Create the RHS vector
+    rhs_vector = vstack((C, d))
+
     
 def estimate_position(accel):
     ''' Estimate the trajectory using information from the acceleration values.
