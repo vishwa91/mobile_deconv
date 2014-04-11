8b34a318ffb40c2fc3fa364cce0887f32cf4df8d
vishwanath <vishwa..hyd@gmail.com>
2014-04-11 15:52:37 +0530

    Estimating depth using Sum Modified Laplacian. Doesn't seem extremely great.

--- host/scratch.py
+++ host/scratch.py
@@ -10,5 +10,22 @@ if __name__ == '__main__':
 	frame_token = 'S\x00T\x00F\x00R\x00'
 	fstart = 'STFR'
 	fend = 'EDFR'
-	dummy_recv(start_token, end_token, frame_token, 'tmp/focus/tokens.dat')
-	extract_images('tmp/focus/tokens.dat', 100, fstart, fend, 'tmp/focus')
+	#dummy_recv(start_token, end_token, frame_token, 'tmp/focus/tokens.dat')
+	#extract_images('tmp/focus/tokens.dat', 100, fstart, fend, 'tmp/focus/src')
+
+	imdepth = zeros((480, 640))
+	imdepth[:,:] = float('inf')
+	im_max = zeros_like(imdepth)
+	window = 15
+	for i in range(3, 100):
+		print 'Reading image %d'%i
+		im = imread('tmp/focus/src/im%d.bmp'%i, flatten=True)
+		imlap = abs(laplace(im))
+		mfilter = ones((window, window), dtype=float)/(8.0*8.0)
+		imlap = linear_convolve(imlap, mfilter)
+		x, y = where(imlap > im_max)
+		im_max[x, y] = imlap[x, y]
+		imdepth[x, y] = i
+		Image.fromarray((imlap*255.0/imlap.max()).astype(uint8)).save(
+			'tmp/focus/sml/im%d.bmp'%i)
+	Image.fromarray(imdepth).convert('RGB').save('tmp/focus/imdepth.bmp')
