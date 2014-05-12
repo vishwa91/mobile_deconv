ade8785bd44494c88df3e61a9123d2f521bda04b
Vishwanath <vishwa.hyd@gmail.com>
2014-05-12 16:57:44 +0530

    Lot to edit.

--- thesis/codes/drift_example.py
+++ thesis/codes/drift_example.py
@@ -6,7 +6,7 @@ from scipy.integrate import *
 import numpy as np
 
 from matplotlib.pyplot import *
-
+G =9.8; T=10e-3;
 if __name__ == '__main__':
 	# Create position by low pass filtering random data.
 	theta = linspace(0,10*pi, 50)
@@ -15,14 +15,16 @@ if __name__ == '__main__':
 	posmax = abs(random_data).max()
 	accel = diff(diff(random_data))
 
-	subplot(2,1,1); plot(cumtrapz(cumtrapz(accel)), label='cumtrapz')
+	subplot(2,1,1); plot(cumtrapz(cumtrapz(accel))*G*T*T)
+	ylabel('meters'); xlabel('samples')
 	title('Actual trajectory')
 	# Create drifts
 	for drift in linspace(-0.01*posmax, 0.01*posmax, 9):
 		temp_accel = accel + drift
 		subplot(2,1,2)
-		plot(cumtrapz(cumtrapz(temp_accel)), label='Drift=%f'%drift)
+		plot(cumtrapz(cumtrapz(temp_accel))*G*T*T, label='Drift=%f'%drift)
 	subplot(2,1,2); title('Trajectories with drif in acceleration')
+	ylabel('meters'); xlabel('samples')
 	legend(loc='upper left', prop={'size':8})
 	savefig('drift_image.png', dpi=250)
 	show()
